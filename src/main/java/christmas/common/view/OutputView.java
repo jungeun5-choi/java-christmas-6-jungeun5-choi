4aa497904041350ee0bd15d1b447c047f3a979dd
jungeun-choi <jungeun5.choi@gmail.com>
2023-11-15 06:56:33 +0900

    feat(outputView): 최소 주문 금액 안내 메세지 출력

--- src/main/java/christmas/common/view/OutputView.java
+++ src/main/java/christmas/common/view/OutputView.java
@@ -14,6 +14,7 @@ public class OutputView {
     }
 
     private static final String LINE_SEPARATOR = System.lineSeparator();
+
     private static final int MONTH = 12;
     private static final String REWARD_ITEM = "샴페인";
     private static final int REWARD_ITEM_COUNT = 1;
@@ -37,30 +38,37 @@ public class OutputView {
             System.out.printf(Message.PRINT_ORDER_LIST.message + LINE_SEPARATOR,
                     entry.getKey(), entry.getValue());
         }
+        System.out.printf(LINE_SEPARATOR);
     }
 
     private void printTotalOrderAmount(OrderDto orderDto) {
         System.out.printf(Message.PRINT_TOTAL_ORDER_AMOUNT_TITLE.message + LINE_SEPARATOR);
-        System.out.printf(Message.PRINT_TOTAL_ORDER_AMOUNT.message + LINE_SEPARATOR, orderDto.getTotalOrderAmount());
+        System.out.printf(Message.PRINT_TOTAL_ORDER_AMOUNT.message + LINE_SEPARATOR,
+                orderDto.getTotalOrderAmount());
+        System.out.printf(LINE_SEPARATOR);
     }
 
     public void printReward(boolean hasReward) {
         System.out.printf(Message.PRINT_REWARD_TITLE.message + LINE_SEPARATOR);
         if (hasReward) {
-            System.out.printf(Message.PRINT_REWARD.message + LINE_SEPARATOR, REWARD_ITEM, REWARD_ITEM_COUNT);
+            System.out.printf(Message.PRINT_REWARD.message + LINE_SEPARATOR,
+                    REWARD_ITEM, REWARD_ITEM_COUNT);
         }
         if (!hasReward) {
             System.out.printf(Message.PRINT_ADVANTAGE_EMPTY.message + LINE_SEPARATOR);
         }
+        System.out.printf(LINE_SEPARATOR);
     }
 
     public void printAdvantageList(List<EventData> eventData, Map<EventType, Integer> discounts, boolean hasAdvantage) {
         System.out.printf(Message.PRINT_ADVANTAGE_LIST_TITLE.message + LINE_SEPARATOR);
         if (hasAdvantage) {
             printAllAdvantages(eventData, discounts);
+            System.out.print(LINE_SEPARATOR);
         }
         if (!hasAdvantage) {
             System.out.printf(Message.PRINT_ADVANTAGE_EMPTY.message + LINE_SEPARATOR);
+            System.out.println(Message.ANNOUNCE_REQUIRE_AMOUNT_FOR_ADVANTAGE.message + LINE_SEPARATOR);
         }
     }
 
@@ -74,11 +82,13 @@ public class OutputView {
     public void printAdvantageAmount(int amount) {
         System.out.printf(Message.PRINT_ADVANTAGE_AMOUNT_TITLE.message + LINE_SEPARATOR);
         System.out.printf(Message.PRINT_ADVANTAGE_AMOUNT.message + LINE_SEPARATOR, amount);
+        System.out.print(LINE_SEPARATOR);
     }
 
     public void printFinalPayment(int amount) {
         System.out.printf(Message.PRINT_FINAL_PAYMENT_TITLE.message + LINE_SEPARATOR);
         System.out.printf(Message.PRINT_FINAL_PAYMENT.message + LINE_SEPARATOR, amount);
+        System.out.print(LINE_SEPARATOR);
     }
 
     public void printExceptionMessage(Exception exception) {
@@ -88,26 +98,28 @@ public class OutputView {
     public void printBadge(String badgeName) {
         System.out.printf(Message.PRINT_BADGE_TITLE.message + LINE_SEPARATOR);
         System.out.printf(Message.PRINT_BADGE.message + LINE_SEPARATOR, badgeName);
+        System.out.print(LINE_SEPARATOR);
     }
 
     private enum Message {
         PRINT_WELCOME("안녕하세요! 우테코 식당 %s월 이벤트 플래너입니다."),
-        PRINT_ADVANTAGE_PREVIEW("\n%s월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
-        PRINT_ORDER_LIST_TITLE("\n<주문 메뉴>"),
+        PRINT_ADVANTAGE_PREVIEW("%s월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
+        PRINT_ORDER_LIST_TITLE("<주문 메뉴>"),
         PRINT_ORDER_LIST("%s %s개"),
-        PRINT_TOTAL_ORDER_AMOUNT_TITLE("\n<할인 전 총주문 금액>"),
+        PRINT_TOTAL_ORDER_AMOUNT_TITLE("<할인 전 총주문 금액>"),
         PRINT_TOTAL_ORDER_AMOUNT("%s원"),
-        PRINT_REWARD_TITLE("\n<증정 메뉴>"),
+        PRINT_REWARD_TITLE("<증정 메뉴>"),
         PRINT_REWARD("%s %s개"),
-        PRINT_ADVANTAGE_LIST_TITLE("\n<혜택 내역>"),
+        PRINT_ADVANTAGE_LIST_TITLE("<혜택 내역>"),
         PRINT_ADVANTAGE_LIST("%s: -%s원"),
-        PRINT_ADVANTAGE_AMOUNT_TITLE("\n<총혜택 금액>"),
+        PRINT_ADVANTAGE_AMOUNT_TITLE("<총혜택 금액>"),
         PRINT_ADVANTAGE_AMOUNT("-%s원"),
-        PRINT_FINAL_PAYMENT_TITLE("\n<할인 후 예상 결제 금액>"),
+        PRINT_FINAL_PAYMENT_TITLE("<할인 후 예상 결제 금액>"),
         PRINT_FINAL_PAYMENT("%s원"),
-        PRINT_BADGE_TITLE("\n<12월 이벤트 배지>"),
+        PRINT_BADGE_TITLE("<12월 이벤트 배지>"),
         PRINT_BADGE("%s"),
         PRINT_ADVANTAGE_EMPTY("없음"),
+        ANNOUNCE_REQUIRE_AMOUNT_FOR_ADVANTAGE("* 총 주문 금액 10,000원 이상부터 이벤트가 적용됩니다."),
         ;
 
         private final String message;
